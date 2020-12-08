f8bd6ff8a78b10e78e2aab6f563b95f151e41e65
DESKTOP-VBMVJ7Q\Dominic <idig0245@scs.ubbcluj.ro>
2020-12-08 23:55:49 +0200

    first commit

---
+++ app/src/main/java/com/example/bitcointrader/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.bitcointrader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQueue = Volley.newRequestQueue(this);
+        TextView tv1 = (TextView) findViewById(R.id.actualPrice);
+        String url = "http://localhost:8081/getActual";
+        jsonParse();
+    }
+
+    private void jsonParse() {
+        System.out.println("jk");
+        String url = "http://192.168.56.1:8081/getActual";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String json = response.toString();
+                            //mTextViewResult.append(json);
+
+                            JSONObject jObject = new JSONObject(json);
+                            System.out.println(json);
+
+                            //Keyword - Value Pairs
+                            String idValue = jObject.getString("price");
+                            TextView tv1 = (TextView) findViewById(R.id.actualPrice);
+                            tv1.setText(idValue);
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, (Response.ErrorListener) error -> {
+                    System.out.println("----------------------");
+                    error.printStackTrace();
+                });
+        mQueue.add(request);
+    }
+}
