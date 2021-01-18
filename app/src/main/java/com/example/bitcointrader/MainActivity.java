df26a63eb4e6c911e7306c205a0368ac5786691e
DESKTOP-VBMVJ7Q\Dominic <idig0245@scs.ubbcluj.ro>
2021-01-18 17:28:17 +0200

    fixed chart for bitcoinprice in the last month. needs styling!!!

--- app/src/main/java/com/example/bitcointrader/MainActivity.java
+++ app/src/main/java/com/example/bitcointrader/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.bitcointrader;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,30 +10,65 @@ import android.widget.TextView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private RequestQueue requestQueue;
     private String url = "http://192.168.56.1:8081";
+    private List<Bitcoin> chartBitcoins = new ArrayList<>();
+    private Bitcoin anualMin;
+    private Bitcoin anualMax;
+    private Bitcoin max;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        requestQueue = Volley.newRequestQueue(this);
+        getValues();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        drawChart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        onPause();
         requestQueue = Volley.newRequestQueue(this);
         refreshActualValue();
-        getValues();
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onResume();
+
+                System.out.println("-------"+chartBitcoins.size());
+            }
+        },1000);
+//        drawChart();
     }
 
     public void refreshActualValue(){
@@ -83,9 +118,9 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             Gson gson = new Gson();
                             String json = response.toString();
-                            Bitcoin max = gson.fromJson(json, Bitcoin.class);
-                            TextView maxPrice = (TextView) findViewById(R.id.maxPrice);
-                            maxPrice.setText(max.showPrice());
+                            max = gson.fromJson(json, Bitcoin.class);
+//                            TextView maxPrice = (TextView) findViewById(R.id.maxPrice);
+//                            maxPrice.setText(max.showPrice());
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -99,9 +134,9 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             Gson gson = new Gson();
                             String json = response.toString();
-                            Bitcoin anualmax = gson.fromJson(json, Bitcoin.class);
-                            TextView anualMaxPrice = (TextView) findViewById(R.id.anualMax);
-                            anualMaxPrice.setText(anualmax.showPrice());
+                            anualMax = gson.fromJson(json, Bitcoin.class);
+//                            TextView anualMaxPrice = (TextView) findViewById(R.id.anualMax);
+//                            anualMaxPrice.setText(anualmax.showPrice());
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -115,14 +150,54 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             Gson gson = new Gson();
                             String json = response.toString();
-                            Bitcoin anualmin= gson.fromJson(json, Bitcoin.class);
-                            TextView anualMinPrice = (TextView) findViewById(R.id.anualMin);
-                            anualMinPrice.setText(anualmin.showPrice());
+                            anualMin= gson.fromJson(json, Bitcoin.class);
+//                            TextView anualMinPrice = (TextView) findViewById(R.id.anualMin);
+//                            anualMinPrice.setText(anualmin.showPrice());
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
                 }, (Response.ErrorListener) Throwable::printStackTrace);
         requestQueue.add(request);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url+"/getLastMonth", null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            Gson gson = new Gson();
+                            String json = response.toString();
+                            Bitcoin[] lastYear= gson.fromJson(json, Bitcoin[].class);
+                            chartBitcoins.addAll(Arrays.asList(lastYear));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, (Response.ErrorListener) Throwable::printStackTrace);
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    public void drawChart(){
+
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+
+
+        List<Entry> entries = new ArrayList<Entry>();
+        int i =0;
+        for (Bitcoin data : chartBitcoins) {
+            i++;
+            // turn your data into Entry objects
+
+            entries.add(new Entry(i, (float)data.getPrice()));
+        }
+
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        LineData lineData = new LineData(dataSet);
+        dataSet.setDrawFilled(true);
+        dataSet.setFillDrawable(ContextCompat.getDrawable(this, R.drawable.gradient));
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
+
     }
 }
