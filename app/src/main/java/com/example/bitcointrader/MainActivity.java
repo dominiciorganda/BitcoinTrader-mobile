18c678878e8702e773a1c4138c3994ee8f4dbe39
DESKTOP-VBMVJ7Q\Dominic <idig0245@scs.ubbcluj.ro>
2021-01-18 23:02:26 +0200

    styled the chart

--- app/src/main/java/com/example/bitcointrader/MainActivity.java
+++ app/src/main/java/com/example/bitcointrader/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.bitcointrader;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,9 +17,12 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
@@ -32,7 +38,7 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     private RequestQueue requestQueue;
-    private String url = "http://192.168.56.1:8081";
+    private String url = "http://192.168.56.1:8081/BitcoinTrader";
     private List<Bitcoin> chartBitcoins = new ArrayList<>();
     private Bitcoin anualMin;
     private Bitcoin anualMax;
@@ -65,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 onResume();
 
-                System.out.println("-------"+chartBitcoins.size());
+//                System.out.println("-------"+chartBitcoins.size());
             }
         },1000);
 //        drawChart();
@@ -119,8 +125,8 @@ public class MainActivity extends AppCompatActivity {
                             Gson gson = new Gson();
                             String json = response.toString();
                             max = gson.fromJson(json, Bitcoin.class);
-//                            TextView maxPrice = (TextView) findViewById(R.id.maxPrice);
-//                            maxPrice.setText(max.showPrice());
+                            TextView maxPrice = (TextView) findViewById(R.id.maxPrice);
+                            maxPrice.setText(max.showPrice());
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -135,8 +141,8 @@ public class MainActivity extends AppCompatActivity {
                             Gson gson = new Gson();
                             String json = response.toString();
                             anualMax = gson.fromJson(json, Bitcoin.class);
-//                            TextView anualMaxPrice = (TextView) findViewById(R.id.anualMax);
-//                            anualMaxPrice.setText(anualmax.showPrice());
+                            TextView anualMaxPrice = (TextView) findViewById(R.id.anualMax);
+                            anualMaxPrice.setText(anualMax.showPrice());
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -151,8 +157,8 @@ public class MainActivity extends AppCompatActivity {
                             Gson gson = new Gson();
                             String json = response.toString();
                             anualMin= gson.fromJson(json, Bitcoin.class);
-//                            TextView anualMinPrice = (TextView) findViewById(R.id.anualMin);
-//                            anualMinPrice.setText(anualmin.showPrice());
+                            TextView anualMinPrice = (TextView) findViewById(R.id.anualMin);
+                            anualMinPrice.setText(anualMin.showPrice());
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -186,18 +192,54 @@ public class MainActivity extends AppCompatActivity {
         int i =0;
         for (Bitcoin data : chartBitcoins) {
             i++;
-            // turn your data into Entry objects
 
             entries.add(new Entry(i, (float)data.getPrice()));
         }
 
 
-        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        LineDataSet dataSet = new LineDataSet(entries,"");
         LineData lineData = new LineData(dataSet);
         dataSet.setDrawFilled(true);
         dataSet.setFillDrawable(ContextCompat.getDrawable(this, R.drawable.gradient));
+        dataSet.setDrawCircles(true);
+        dataSet.setDrawCircleHole(true);
+        dataSet.setCircleHoleColor(Color.BLACK);
+        dataSet.setCircleRadius(10);
+        dataSet.setCircleHoleRadius(8);
+        dataSet.setValueTextSize(0);
+        dataSet.setDrawHorizontalHighlightIndicator(false);
+        dataSet.setDrawVerticalHighlightIndicator(false);
+
+
+
+
         chart.setData(lineData);
+        chart.getAxisLeft().setDrawGridLines(false);
+        chart.getXAxis().setDrawGridLines(false);
+        chart.getAxisRight().setDrawGridLines(false);
+        chart.getDescription().setEnabled(false);
+        chart.getAxisLeft().setDrawLabels(false);
+        chart.getAxisRight().setDrawLabels(false);
+        chart.getXAxis().setDrawLabels(false);
+        chart.getLegend().setEnabled(false);
         chart.invalidate(); // refresh
+
+        chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                Toast.makeText(MainActivity.this, "The text you want to display", Toast.LENGTH_LONG).show();
+
+                float x=e.getX();
+                float y=e.getY();
+                System.out.println(x);
+                System.out.println(y);
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
 
     }
 }
